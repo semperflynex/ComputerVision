2db3efa3c82a10301123cb9b4794313de58eb453
Adam Mahmoud <adammahmoud96@live.de>
2018-12-05 11:29:37 +0100

    It Works

---
+++ ImageStitching/WarpImage.m
@@ -0,0 +1,10 @@
+function [I2] = WarpImage(I1,H)
+%WARPIMAGE Summary of this function goes here
+%   Detailed explanation goes here
+
+%Create an Array with Bounds of Transformed Image
+%filled with 0 (Black)
+
+
+end
+
